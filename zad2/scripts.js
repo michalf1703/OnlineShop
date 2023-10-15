1f8f678f38a3d92ff3a9b77f18e27d1e90c94657
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-10-15 20:12:57 +0200

    3a done

--- zad2/scripts.js
+++ zad2/scripts.js
@@ -1,2 +1,42 @@
 "use strict"
 let todoList = []; //declares a new array for Your todo list
+
+let initList = function() {
+    todoList.push(
+        {
+            title: "Learn JS",
+            description: "Create a demo application for my TODO's",
+            place: "445",
+            dueDate: new Date(2019,10,16)
+        },
+        {
+            title: "Lecture test",
+            description: "Quick test from the first three lectures",
+            place: "F6",
+            dueDate: new Date(2019,10,17)
+        }
+        // of course the lecture test mentioned above will not take place
+    );
+}
+
+initList();
+let updateTodoList = function() {
+    let todoListDiv =
+        document.getElementById("todoListView");
+
+    //remove all elements
+    while (todoListDiv.firstChild) {
+        todoListDiv.removeChild(todoListDiv.firstChild);
+    }
+
+    //add all elements
+    for (let todo in todoList) {
+        let newElement = document.createElement("div");
+        let newContent = document.createTextNode(
+            todoList[todo].title + " " + todoList[todo].description);
+        newElement.appendChild(newContent);
+        todoListDiv.appendChild(newElement);
+    }
+}
+
+setInterval(updateTodoList, 1000);
