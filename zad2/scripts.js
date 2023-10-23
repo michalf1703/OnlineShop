ca9be65abd836dbcc467aa046ed8234cf4090b04
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-10-23 22:26:21 +0200

    end task2

--- zad2/scripts.js
+++ zad2/scripts.js
@@ -1,125 +1,146 @@
 "use strict"
 let todoList = []; //declares a new array for Your todo list
 
-let initList = function() {
-    let savedList = window.localStorage.getItem("todos");
-    if (savedList != null)
-        todoList = JSON.parse(savedList);
-    else
-//code creating a default list with 2 items
-    todoList.push(
-        {
-            title: "Learn JS",
-            description: "Create a demo application for my TODO's",
-            place: "445",
-            dueDate: new Date(2019,10,16)
-        },
-        {
-            title: "Lecture test",
-            description: "Quick test from the first three lectures",
-            place: "F6",
-            dueDate: new Date(2019,10,17)
-        }
-        // of course the lecture test mentioned above will not take place
-    );
-}
-
 const BASE_URL = "https://api.jsonbin.io/v3/b/65369a070574da7622bca6a9";
 const SECRET_KEY = "$2a$10$QEem8jJt/wGqycGeFsRSFe7VST/uZ.xVek6cSbABLUF2od1.k6u3e";
-$.ajax({
-    // copy Your bin identifier here. It can be obtained in the dashboard
-    url: BASE_URL,
-    type: 'GET',
-    headers: { //Required only if you are trying to access a private bin
-        'X-Master-Key': SECRET_KEY
-    },
-    success: (data) => {
-        //console.log(data);
-        todoList = data;
-    },
-    error: (err) => {
-        console.log(err.responseJSON);
-    }
-});
+const isDueDateAMatch = (from, to, dueDate) => {
+    const areBothDatesProvided = from && to;
+    const isAnyDateProvided = from || to;
 
-//initList();
-let updateTodoList = function() {
-    let todoListDiv =
-        document.getElementById("todoListView");
+    const isDueDateBeforeEndDate = dueDate < to;
+    const isDueDateAfterStartDate = from ? dueDate > from : false;
 
-    //remove all elements
-    while (todoListDiv.firstChild) {
-        todoListDiv.removeChild(todoListDiv.firstChild);
+    const isDueDateBetweenDates =
+        isDueDateBeforeEndDate && isDueDateAfterStartDate;
+
+    if (!isAnyDateProvided) {
+        return true;
     }
 
-//add all elements
-    let filterInput = document.getElementById("inputSearch");
-    for (let todo in todoList) {
-        if (
-            (filterInput.value == "") ||
-            (todoList[todo].title.includes(filterInput.value)) ||
-            (todoList[todo].description.includes(filterInput.value))
-        ) {
-            let newElement = document.createElement("p");
-            let newContent = document.createTextNode(todoList[todo].title + " " +
-                todoList[todo].description);
-            newElement.appendChild(newContent);
-            todoListDiv.appendChild(newElement);
-        }
+    if (areBothDatesProvided) {
+        return isDueDateBetweenDates;
     }
-    let newDeleteButton = document.createElement("input");
-    newDeleteButton.type = "button";
-    newDeleteButton.value = "x";
-    newDeleteButton.addEventListener("click",
-        function() {
-            deleteTodo(todo);
-        });
-}
 
-setInterval(updateTodoList, 1000);
+    if (isAnyDateProvided) {
+        return isDueDateBeforeEndDate || isDueDateAfterStartDate;
+    }
 
-let deleteTodo = function(index) {
-    todoList.splice(index,1);
-    updateJSONbin();
-}
-let addTodo = function() {
-    //get the elements in the form
-    let inputTitle = document.getElementById("inputTitle");
-    let inputDescription = document.getElementById("inputDescription");
-    let inputPlace = document.getElementById("inputPlace");
-    let inputDate = document.getElementById("inputDate");
-    //get the values from the form
-    let newTitle = inputTitle.value;
-    let newDescription = inputDescription.value;
-    let newPlace = inputPlace.value;
-    let newDate = new Date(inputDate.value);
-    //create new item
-    let newTodo = {
-        title: newTitle,
-        description: newDescription,
-        place: newPlace,
-        dueDate: newDate
-    };
-    //add item to the list
-    todoList.push(newTodo);
-    window.localStorage.setItem("todos", JSON.stringify(todoList));
-    updateJSONbin();
-}
-newElement.appendChild(newDeleteButton);
+    return false;
+};
 
-let updateJSONbin = function() {
+const isSearchAMatch = (filterValue, title, description) => {
+    const isFilterEmpty = filterValue === "";
+    const doesTitleIncludeFilter = title.includes(filterValue);
+    const doesDescriptionIncludeFilter = description.includes(filterValue);
+
+    return (
+        isFilterEmpty || doesTitleIncludeFilter || doesDescriptionIncludeFilter
+    );
+};
+
+(() => {
     $.ajax({
         url: BASE_URL,
-        type: 'PUT',
-        headers: { //Required only if you are trying to access a private bin
+        type: "GET",
+        success: (data) => {
+            todoList = data.record;
+        },
+        error: (err) => {
+            console.log(err.responseJSON);
+        },
+    });
+})();
+
+let updateJSONbin = function () {
+    $.ajax({
+        url: BASE_URL,
+        type: "PUT",
+        headers: {
             'X-Master-Key': SECRET_KEY
         },
-        contentType: 'application/json',
+        contentType: "application/json",
         data: JSON.stringify(todoList),
         success: (data) => {
             console.log(data);
         },
         error: (err) => {
             console.log(err.responseJSON);
+        },
+    });
+};
+
+// updating todos
+const updateTodoList = function () {
+    const todoListDiv = $("#todoListTable")[0];
+
+    // clear todos before updating
+    while (todoListDiv.firstChild) {
+        todoListDiv.removeChild(todoListDiv.firstChild);
+    }
+
+    const filterInput = $("#inputSearch")[0];
+
+    // create new todos for the update
+    for (const todo in todoList) {
+        const from = $("#startDate")[0].value;
+        const to = $("#endDate")[0].value;
+        const dueDate = todoList[todo].dueDate;
+
+        const filterValue = filterInput.value;
+        const title = todoList[todo].title;
+        const description = todoList[todo].description;
+
+        // render elements only if they fit the filter value
+
+        if (
+            isSearchAMatch(filterValue, title, description) &&
+            isDueDateAMatch(from, to, dueDate)
+        ) {
+            const newElement = $(`<tr></tr>`)
+                .append($(`<td>${todoList[todo].title}</td>`))
+                .append($(`<td>${todoList[todo].description}</td>`))
+                .append($(`<td>${todoList[todo].place}</td>`))
+                .append($(`<td>${todoList[todo].dueDate}</td>`))[0];
+
+            newElement.append(
+                $(
+                    '<td><button type="submit" class="btn btn-danger pt-0">x</button><td>'
+                ).click(() => {
+                    deleteTodo(todoList.indexOf(todo));
+                })[0]
+            );
+
+            // add new todo
+            todoListDiv.append(newElement);
         }
-    })};
+    }
+};
+
+setInterval(updateTodoList, 1000);
+
+
+const deleteTodo = function (index) {
+    // remove todo
+    todoList.splice(index, 1);
+    updateJSONbin();
+
+};
+
+const addTodo = function () {
+    // get the values from the form
+    const newTitle = $("#inputTitle")[0].value;
+    const newDescription = $("#inputDescription")[0].value;
+    const newPlace = $("#inputPlace")[0].value;
+    const newDate = $("#inputDate")[0].value;
+    // create new Todo
+    const newTodo = {
+        title: newTitle,
+        description: newDescription,
+        place: newPlace,
+        dueDate: newDate,
+    };
+    // add todo to the list
+    todoList.push(newTodo);
+    // update todos on the backend
+    updateJSONbin();
+};
