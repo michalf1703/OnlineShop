b11ae311617e17ae90dfd9249b69edf7322f7dbf
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-11-21 01:52:17 +0100

    cos tam dziala

--- zad3/src/App.vue
+++ zad3/src/App.vue
@@ -1,12 +1,37 @@
 <template>
-  <p>XDDD</p>
+  <h3>
+    <Search />
+    <MoviesTable />
+    <GenreMoviesList />
+    <CastMoviesList />
+  </h3>
 </template>
 
 <script>
+import Search from "./components/Search.vue";
+import MoviesTable from "./components/MoviesTable.vue";
+import GenreMoviesList from "./components/GenreMoviesList.vue";
+import CastMoviesList from "./components/CastMoviesList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
+    Search,
+    MoviesTable,
+    GenreMoviesList,
+    CastMoviesList,
   }
-}
+};
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin: 1rem auto;
+  padding: 0 10rem;
+}
+</style>
--- zad3/src/components/MoviesTable.vue
+++ zad3/src/components/MoviesTable.vue
@@ -1,57 +1,57 @@
 <template>
-    <div>
-      <h5>Wyświetlanie obecnie {{ howManyLoaded }} / {{ movies.length }}</h5>
-      <table class="table-condensed table-hover">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Production Year</th>
-            <th>Cast</th>
-            <th>Genres</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(movie, index) in moviesToDisplay" :key="index">
-            <td>{{ movie.title }}</td>
-            <td>{{ movie.year }}</td>
-            <td>{{ movie.cast.join(", ") }}</td>
-            <td>{{ movie.genres.join(", ") }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <div v-if="howManyLoaded <= movies.length">
-        <button class="btn btn-info col-sm-12" v-on:click="loadMore">
-          Load More
-        </button>
-      </div>
+  <div>
+    <h5>Wyświetlanie obecnie {{ howManyLoaded }} / {{ movies.length }}</h5>
+    <table class="table-condensed table-hover">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Production Year</th>
+          <th>Cast</th>
+          <th>Genres</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(movie, index) in moviesToDisplay" :key="index">
+          <td>{{ movie.title }}</td>
+          <td>{{ movie.year }}</td>
+          <td>{{ movie.cast.join(", ") }}</td>
+          <td>{{ movie.genres.join(", ") }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div v-if="howManyLoaded <= movies.length">
+      <button class="btn btn-info col-sm-12" v-on:click="loadMore">
+        Load More
+      </button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "MoviesTable",
-    data() {
-      return {
-        movies: [],
-        howManyLoaded: 0,
-        moviesToDisplay: [],
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MoviesTable",
+  data() {
+    return {
+      movies: [],
+      howManyLoaded: 0,
+      moviesToDisplay: [],
+    };
+  },
+  methods: {
+    loadMore() {
+      this.howManyLoaded += 10;
+      this.moviesToDisplay = this.movies.slice(0, this.howManyLoaded);
     },
-    methods: {
-      loadMore() {
-        this.howManyLoaded += 10;
-        this.moviesToDisplay = this.movies.slice(0, this.howManyLoaded);
-      },
-    },
-    created() {
-      this.$emitter.emit("update-data");
-      this.$emitter.on("search-change-params", (movies) => {
-        this.movies = movies;
-        this.moviesToDisplay = this.movies.slice(0, this.howManyLoaded);
-        if (this.howManyLoaded < 10) this.loadMore();
-      });
-    },
-  };
-  </script>
-  
-  <style></style>
+  },
+  created() {
+    this.$emitter.emit("update-data");
+    this.$emitter.on("search-change-params", (movies) => {
+      this.movies = movies;
+      this.moviesToDisplay = this.movies.slice(0, this.howManyLoaded);
+      if (this.howManyLoaded < 10) this.loadMore();
+    });
+  },
+};
+</script>
+
+<style></style>
